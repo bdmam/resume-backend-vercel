c8274bf5a51e41810a70d055781a72c100e3a96c
bdmam <39810845+bdmam@users.noreply.github.com>
2025-08-09 08:14:33 -0500

    Update ga4_config.js

--- api/ga4_config.js
+++ api/ga4_config.js
@@ -1,4 +1,7 @@
-// api/ga4_config.js
 export default async function handler(req, res){
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.status(204).end();
   res.status(200).json({ measurementId: process.env.GA4_ID || "" });
 }
